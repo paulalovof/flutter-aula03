c0f2083b12154559d14b2bb31682dc94f220ae7a
paulalovof <paulafernanda.lovo@gmail.com>
2024-03-26 09:24:34 -0300

    initial commit

---
+++ jokempo/android/app/src/main/kotlin/com/example/jokempo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jokempo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ jokempo/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:jokempo/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Jokempo", debugShowCheckedModeBanner: false, home: HomePage()));
+}
